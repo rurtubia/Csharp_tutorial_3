966d8dc8fecff94a50caabe450a74dd01f2f2801
rurtubiac <randy.urtubia@inacapmail.cl>
2015-08-13 13:33:07 -0300

    Added superheroes code

--- Csharp_tutorial3/Program.cs
+++ Csharp_tutorial3/Program.cs
@@ -18,6 +18,51 @@ namespace Csharp_tutorial3
             Console.WriteLine("GoodBye World");
             Console.ReadLine();
 
+            Console.WriteLine("Type a superhero's name to see his nickname:");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            //In C# a <> is called a generic method.
+            //Parameters:
+            //1st:string to lookup
+            //2nd:boolean to ignore capitalization
+            //3rd:output parameter, returned from the TryParse
+
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue))
+            {
+                //myValue [enter] [enter] lists all the values in the corresponding
+                //enumeration
+
+                switch (myValue)
+                {
+                    case SuperHero.Batman:
+                        Console.WriteLine("Caped Crusader");
+                        break;
+                    case SuperHero.Superman:
+                        Console.WriteLine("Man of Steel");
+                        break;
+                    case SuperHero.GreenLantern:
+                        Console.WriteLine("Emerald Knight");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Does not compute");
+            }
+
+            Console.ReadLine();
+
         }
+    }
+
+    enum SuperHero
+    { 
+        Batman,
+        Superman,
+        GreenLantern
     }
 }
