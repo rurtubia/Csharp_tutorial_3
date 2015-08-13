344bacdb53218d1cebed5b996652936eed22b780
rurtubiac <randy.urtubia@inacapmail.cl>
2015-08-13 13:20:21 -0300

    initial commit

---
+++ Csharp_tutorial3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_tutorial3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Hello World");
+
+
+        }
+    }
+}
